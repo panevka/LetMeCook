7e8cac95c70abc2b28338b8c9e2472c510dbe09e
panevka <97028404+panevka@users.noreply.github.com>
2025-10-05 11:53:04 +0200

    feat(mobile): create draft project list view

--- app/composeApp/src/commonMain/kotlin/com/letmecook/app/App.kt
+++ app/composeApp/src/commonMain/kotlin/com/letmecook/app/App.kt
@@ -1,49 +1,108 @@
 package com.letmecook.app
 
-import androidx.compose.animation.AnimatedVisibility
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.FlowRow
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.safeContentPadding
-import androidx.compose.material3.Button
+import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.ElevatedCard
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.SegmentedButtonDefaults.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import org.jetbrains.compose.resources.painterResource
-import org.jetbrains.compose.ui.tooling.preview.Preview
-
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
 import app.composeapp.generated.resources.Res
 import app.composeapp.generated.resources.compose_multiplatform
+import org.jetbrains.compose.ui.tooling.preview.Preview
+
+import com.letmecook.domain.project.PaymentType
+import com.letmecook.domain.project.ProjectEntity
+import org.jetbrains.compose.resources.painterResource
 
 @Composable
 @Preview
 fun App() {
+
+    fun initMockData(): List<ProjectEntity> {
+        val tags = arrayOf("Open-source", "Startup", "Teenagers", "Non-tech tag");
+
+        val projects = (1..5).map { project ->
+            ProjectEntity(
+                "Project $project",
+                "Super-duper, hyper, gigachad, sigma description $project",
+                tags.map { tag -> "$tag $project" }.toTypedArray(),
+                PaymentType.values().random()
+            )
+        }
+        return projects;
+    }
+
+    val mockProjects = initMockData();
+
     MaterialTheme {
-        var showContent by remember { mutableStateOf(false) }
         Column(
-            modifier = Modifier
-                .background(MaterialTheme.colorScheme.primaryContainer)
-                .safeContentPadding()
-                .fillMaxSize(),
+            modifier = Modifier.background(MaterialTheme.colorScheme.primaryContainer).safeContentPadding()
+                .padding(20.dp).fillMaxWidth(),
+
             horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.spacedBy(15.dp),
         ) {
-            Button(onClick = { showContent = !showContent }) {
-                Text("Click me!")
-            }
-            AnimatedVisibility(showContent) {
-                val greeting = remember { Greeting().greet() }
-                Column(
-                    modifier = Modifier.fillMaxWidth(),
-                    horizontalAlignment = Alignment.CenterHorizontally,
-                ) {
-                    Image(painterResource(Res.drawable.compose_multiplatform), null)
-                    Text("Compose: $greeting")
+            LazyColumn(
+                verticalArrangement = Arrangement.spacedBy(15.dp)
+            ) {
+                items(mockProjects) { project ->
+
+                    ElevatedCard(
+                        modifier = Modifier.height(200.dp).fillMaxWidth().clip(RoundedCornerShape(percent = 10))
+                            .background(Color.White)
+                    ) {
+                        Column(
+                            modifier = Modifier.padding(15.dp)
+                        ) {
+                            Text(
+                                project.name,
+                                style = MaterialTheme.typography.titleLarge,
+                                modifier = Modifier.padding(0.dp, 0.dp, 0.dp, 5.dp),
+                            )
+                            Text("Payment: ${project.paymentType} ")
+
+                            FlowRow(
+                                modifier = Modifier.wrapContentSize(),
+                                horizontalArrangement = Arrangement.spacedBy(10.dp),
+                                verticalArrangement = Arrangement.spacedBy(5.dp)
+                            ) {
+                                project.tags.map { tag ->
+
+                                    Text(
+                                        text = tag,
+                                        color = Color.White,
+                                        modifier = Modifier.clip(
+                                            RoundedCornerShape(percent = 50)
+                                        ).background(Color.Blue).padding(5.dp),
+                                        style = MaterialTheme.typography.labelSmall
+                                    )
+
+                                }
+                            }
+                        }
+
+                    }
+
                 }
+
             }
+
         }
     }
 }
