8c808fa918c62dde39aa768ea32b59147b610eb2
panevka <97028404+panevka@users.noreply.github.com>
2025-10-05 12:37:05 +0200

    feat(mobile): add technical stack display for projects

--- app/composeApp/src/commonMain/kotlin/com/letmecook/app/App.kt
+++ app/composeApp/src/commonMain/kotlin/com/letmecook/app/App.kt
@@ -4,31 +4,34 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.FlowRow
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.safeContentPadding
+import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
 import androidx.compose.material3.ElevatedCard
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.SegmentedButtonDefaults.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.AlignmentLine
+import androidx.compose.ui.layout.VerticalAlignmentLine
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
-import app.composeapp.generated.resources.Res
-import app.composeapp.generated.resources.compose_multiplatform
 import org.jetbrains.compose.ui.tooling.preview.Preview
 
 import com.letmecook.domain.project.PaymentType
 import com.letmecook.domain.project.ProjectEntity
-import org.jetbrains.compose.resources.painterResource
+import com.letmecook.domain.project.TechnicalStack
 
 @Composable
 @Preview
@@ -42,7 +45,7 @@ fun App() {
                 "Project $project",
                 "Super-duper, hyper, gigachad, sigma description $project",
                 tags.map { tag -> "$tag $project" }.toTypedArray(),
-                PaymentType.values().random()
+                PaymentType.values().random(), TechnicalStack.values().random()
             )
         }
         return projects;
@@ -75,6 +78,7 @@ fun App() {
                                 style = MaterialTheme.typography.titleLarge,
                                 modifier = Modifier.padding(0.dp, 0.dp, 0.dp, 5.dp),
                             )
+                            Text("Technical stack: ${project.technicalStack} ")
                             Text("Payment: ${project.paymentType} ")
 
                             FlowRow(
