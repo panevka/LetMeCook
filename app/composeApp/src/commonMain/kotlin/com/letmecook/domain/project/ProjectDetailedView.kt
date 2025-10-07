4fc1f487330d985ab3f6e7150a692cbc183c932f
panevka <97028404+panevka@users.noreply.github.com>
2025-10-07 13:33:36 +0200

    feat(mobile): add draft detailed view of chosen project

---
+++ app/composeApp/src/commonMain/kotlin/com/letmecook/domain/project/ProjectDetailedView.kt
@@ -0,0 +1,30 @@
+package com.letmecook.domain.project
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import org.jetbrains.compose.ui.tooling.preview.Preview
+
+@Composable
+@Preview
+fun ProjectDetailedView (project: ProjectEntity) {
+
+    MaterialTheme {
+        Column(
+            modifier = Modifier.fillMaxSize()
+        ) {
+            Text(project.name, style = MaterialTheme.typography.titleLarge)
+            Text(project.description, style = MaterialTheme.typography.titleLarge)
+            Text(project.technicalStack.toString(), style = MaterialTheme.typography.titleLarge)
+            Text(project.paymentType.toString(), style = MaterialTheme.typography.titleLarge)
+            project.tags.map {
+                tag ->
+                Text(tag, style = MaterialTheme.typography.titleLarge)
+            }
+        }
+    }
+
+}
