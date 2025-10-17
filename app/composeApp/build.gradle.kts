7977265375fda9d75f19b21724795cf573cd0072
panevka <97028404+panevka@users.noreply.github.com>
2025-10-17 17:57:40 +0200

    feat: implement navigation between profiler & post view

--- app/composeApp/build.gradle.kts
+++ app/composeApp/build.gradle.kts
@@ -8,6 +8,7 @@ plugins {
     alias(libs.plugins.composeMultiplatform)
     alias(libs.plugins.composeCompiler)
     alias(libs.plugins.composeHotReload)
+    kotlin("plugin.serialization") version "2.2.20"
 }
 
 kotlin {
