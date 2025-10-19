ab9eb303ab681096865c4373eebac4abdecad360
panevka <97028404+panevka@users.noreply.github.com>
2025-10-19 14:54:37 +0200

    feat: create a login & registration system

--- app/composeApp/src/commonMain/kotlin/com/letmecook/domain/account/LoginView.kt
+++ app/composeApp/src/commonMain/kotlin/com/letmecook/domain/account/LoginView.kt
@@ -10,11 +10,17 @@ import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import com.letmecook.app.SignUp
+import com.letmecook.app.currentUser
+import com.letmecook.app.userDb
 import org.jetbrains.compose.ui.tooling.preview.Preview
 import kotlin.math.sign
 
@@ -22,16 +28,29 @@ import kotlin.math.sign
 @Composable
 internal fun LoginView(navController: NavController) {
 
+    var email by remember { mutableStateOf("") }
+    var password by remember { mutableStateOf("") }
+    var text by remember {mutableStateOf("Sign in")}
+
     Column(
         modifier = Modifier.fillMaxSize(),
         horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.Center
     ) {
 
-        TextField(value = "aha", onValueChange = {}, modifier = Modifier.width(300.dp), label = { Text("E-mail") })
-        TextField(value = "aha", onValueChange = {}, modifier = Modifier.width(300.dp), label = { Text("Password") })
+        Text(text)
+        TextField(value = email , onValueChange = { email = it}, modifier = Modifier.width(300.dp), label = { Text("E-mail") })
+        TextField(value = password, onValueChange = { password = it}, modifier = Modifier.width(300.dp), label = { Text("Password") })
 
-        Button(onClick = {}, modifier = Modifier.width(300.dp)) {
+        Button(onClick = {
+            text = if(userDb[email] == password){
+                currentUser = Pair(email, password)
+                "Successful login"
+            } else {
+                "Wrong password"
+            }
+
+        }, modifier = Modifier.width(300.dp)) {
             Text("Log in")
         }
 
