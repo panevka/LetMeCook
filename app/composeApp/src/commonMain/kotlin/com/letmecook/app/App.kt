a85de64478f8e302b2bbcc8a236c3f23b2b6a18c
panevka <97028404+panevka@users.noreply.github.com>
2025-10-19 13:58:18 +0200

    feat: add draft views for sign up & sign in

--- app/composeApp/src/commonMain/kotlin/com/letmecook/app/App.kt
+++ app/composeApp/src/commonMain/kotlin/com/letmecook/app/App.kt
@@ -1,50 +1,41 @@
 package com.letmecook.app
 
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.FlowRow
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.safeContentPadding
-import androidx.compose.foundation.layout.wrapContentSize
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
-import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.BottomAppBar
 import androidx.compose.material3.Button
-import androidx.compose.material3.ElevatedCard
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.*
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.unit.dp
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import com.letmecook.domain.account.LoginView
+import com.letmecook.domain.account.SignUpView
 import org.jetbrains.compose.ui.tooling.preview.Preview
 
-import com.letmecook.domain.project.PaymentType
-import com.letmecook.domain.project.ProjectDetailedView
-import com.letmecook.domain.project.ProjectEntity
 import com.letmecook.domain.project.ProjectListView
-import com.letmecook.domain.project.TechnicalStack
 import com.letmecook.domain.user.UserView
 import kotlinx.serialization.Serializable
 
+
+sealed class Routes {
+
+}
 @Serializable
 object Profile
 
 @Serializable
 object PostList
+
+@Serializable
+object Login
+
+@Serializable
+object SignUp
 
 @Composable
 @Preview
@@ -65,6 +56,9 @@ fun App() {
                         Button(onClick = { navController.navigate(Profile) }) {
                             Text("Profile")
                         }
+                        Button(onClick = { navController.navigate(Login) }) {
+                            Text("Login")
+                        }
                     },
                 )
             }
@@ -77,6 +71,8 @@ fun App() {
             ) {
                 composable<Profile> { UserView() }
                 composable<PostList> { ProjectListView() }
+                composable<Login> { LoginView(navController) }
+                composable<SignUp> { SignUpView(navController) }
             }
 
         }
---
+++ app/composeApp/src/commonMain/kotlin/com/letmecook/domain/account/SignUpView.kt
@@ -0,0 +1,48 @@
+package com.letmecook.domain.account
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
+import androidx.compose.material3.TextField
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.navigation.NavController
+import com.letmecook.app.Login
+import org.jetbrains.compose.ui.tooling.preview.Preview
+
+@Preview
+@Composable
+internal fun SignUpView(navController: NavController){
+
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+
+        TextField(value = "aha", onValueChange = {}, modifier = Modifier.width(300.dp), label = { Text("E-mail") })
+        TextField(value = "aha", onValueChange = {}, modifier = Modifier.width(300.dp), label = { Text("Password") })
+
+        Button(onClick = {}, modifier = Modifier.width(300.dp)) {
+            Text("Sign up")
+        }
+
+        Text("Already have an account?", style = MaterialTheme.typography.bodySmall)
+        TextButton(onClick = { navController.navigate(Login)}){
+            Text(
+                "Sign in",
+                style = MaterialTheme.typography.bodySmall,
+                color = MaterialTheme.colorScheme.primary
+            )
+        }
+
+    }
+
+}
