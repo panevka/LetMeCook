aec6994250e469336d27a4e133d8904e94d7938e
panevka <97028404+panevka@users.noreply.github.com>
2025-10-21 14:43:29 +0200

    chore: initialise spring boot backend repository

---
+++ backend/src/main/java/com/letmecook/backend/LetMeCookBackendApplication.java
@@ -0,0 +1,13 @@
+package com.letmecook.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LetMeCookBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LetMeCookBackendApplication.class, args);
+	}
+
+}
