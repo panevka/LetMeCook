751431956458e09a6d576cba4f07e55728e27184
panevka <97028404+panevka@users.noreply.github.com>
2025-10-21 14:43:29 +0200

    chore: initialise spring boot backend repository

---
+++ backend/src/test/java/com/letmecook/backend/LetMeCookBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.letmecook.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LetMeCookBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
