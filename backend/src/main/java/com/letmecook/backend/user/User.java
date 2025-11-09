afe6cb22471d6ca10f0cc28f4808f276aa9da6dc
panevka <97028404+panevka@users.noreply.github.com>
2025-11-09 14:06:02 +0100

    feat: give User ability to have a firstName

--- backend/src/main/java/com/letmecook/backend/user/User.java
+++ backend/src/main/java/com/letmecook/backend/user/User.java
@@ -3,27 +3,46 @@ package com.letmecook.backend.user;
 class User {
 
     private final String username;
+    private final String firstName;
 
-    private User(String username) {
+    private User(String username, String firstName) {
         if (username == null) {
             throw new IllegalArgumentException("Username cannot be null");
         }
         if (username.isBlank()) {
-            throw new IllegalArgumentException("Username cannot be null or blank");
+            throw new IllegalArgumentException("Username cannot be blank");
         }
         if (username.length() < 3 || username.length() > 20) {
             throw new IllegalArgumentException("Username must be 3-20 characters");
         }
+        if (firstName != null && firstName.isBlank()) {
+            throw new IllegalArgumentException("First name cannot be blank");
+        }
+        if (firstName != null && firstName.length() < 2) {
+            throw new IllegalArgumentException("First name must be at least 2 characters");
+        }
+        if (firstName != null && firstName.length() > 30) {
+            throw new IllegalArgumentException("First name must be at most 30 characters");
+        }
 
         this.username = username;
+        this.firstName = firstName;
     }
 
     static User create(String username) {
-        return new User(username);
+        return new User(username, null);
+    }
+
+    static User create(String username, String firstName) {
+        return new User(username, firstName);
     }
 
     String getUsername() {
         return this.username;
+    }
+
+    String getFirstName() {
+        return this.firstName;
     }
 
 }
