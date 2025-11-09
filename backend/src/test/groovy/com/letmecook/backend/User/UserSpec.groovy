a0283e48979e3b80a817a95fe9906cf31ba9099b
panevka <97028404+panevka@users.noreply.github.com>
2025-11-09 14:06:02 +0100

    feat: give User ability to have a firstName

--- backend/src/test/groovy/com/letmecook/backend/User/UserSpec.groovy
+++ backend/src/test/groovy/com/letmecook/backend/User/UserSpec.groovy
@@ -52,5 +52,65 @@ class UserSpec extends Specification {
                 createdUser.getUsername() == validUsername
         }
 
+        def "A user should be able to have a first name"() {
+                given: "firstName that meets all rules"
+                def validUsername = "Player123"
+                def firstName = "Thomas"
+
+                when:
+                def createdUser = User.create(validUsername, firstName)
+
+                then:
+                createdUser.getFirstName() == firstName
+        }
+
+        def "A user should be able to not have a first name"() {
+                given: "firstName that is null"
+                def validUsername = "Player123"
+                def firstName = null
+
+                when:
+                def createdUser = User.create(validUsername, firstName)
+
+                then:
+                createdUser.getFirstName() == firstName
+        }
+
+        def "A user should not be able to have a blank first name"() {
+                given: "firstName that is blank"
+                def validUsername = "Player123"
+                def firstName = ""
+
+                when:
+                def createdUser = User.create(validUsername, firstName)
+
+                then:
+                def ex = thrown(IllegalArgumentException)
+        }
+
+        def "A user should not be able to have a first name shorter than 2 characters"() {
+                given: "firstName that is blank"
+                def validUsername = "Player123"
+                def tooShortFirstName = "A"
+
+                when:
+                def createdUser = User.create(validUsername, tooShortFirstName)
+
+                then:
+                def ex = thrown(IllegalArgumentException)
+        }
+
+        def "A user should not be able to have a first name longer than 30 characters"() {
+                given: "firstName that is blank"
+                def validUsername = "Player123"
+                def maxLength = 30
+                def tooLongFirstName = "A" * (maxLength + 1)
+
+                when:
+                def createdUser = User.create(validUsername, tooLongFirstName)
+
+                then:
+                def ex = thrown(IllegalArgumentException)
+        }
 
 }
