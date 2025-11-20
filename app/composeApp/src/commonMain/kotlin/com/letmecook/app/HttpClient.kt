22eaa98d5885f384fcd4b0513ff4f984d6499de6
panevka <97028404+panevka@users.noreply.github.com>
2025-11-21 00:03:56 +0100

    feat: setup content negotiation preference as json in Http Client

--- app/composeApp/src/commonMain/kotlin/com/letmecook/app/HttpClient.kt
+++ app/composeApp/src/commonMain/kotlin/com/letmecook/app/HttpClient.kt
@@ -1,14 +1,21 @@
 package com.letmecook.app
 
-import androidx.compose.ui.platform.LocalUriHandler
 import io.ktor.client.HttpClient
+import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
 import io.ktor.client.plugins.defaultRequest
 import io.ktor.client.plugins.resources.Resources
+import io.ktor.serialization.kotlinx.json.json
+import kotlinx.serialization.json.Json
 
 val API_URL = "http://192.168.1.11:8080/api/"
 val client = HttpClient(){
    install(Resources)
-    defaultRequest {
+   install(ContentNegotiation) {
+      json(Json {
+          ignoreUnknownKeys = true
+      })
+   }
+   defaultRequest {
         url(API_URL)
     }
 }
