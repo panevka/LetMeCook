dea1005a43e1820674619e7a3d9ee63ac8bc32ae
panevka <97028404+panevka@users.noreply.github.com>
2025-11-22 00:50:25 +0100

    feat: prettify ProjectListView

--- app/composeApp/src/commonMain/kotlin/com/letmecook/domain/project/ProjectListView.kt
+++ app/composeApp/src/commonMain/kotlin/com/letmecook/domain/project/ProjectListView.kt
@@ -1,7 +1,6 @@
 package com.letmecook.domain.project
 
 import androidx.compose.foundation.background
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -11,43 +10,37 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
-import androidx.compose.material3.DropdownMenu
-import androidx.compose.material3.DropdownMenuItem
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.ElevatedCard
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.FilterChip
-import androidx.compose.material3.InputChip
-import androidx.compose.material3.ListItem
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.SearchBar
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.mutableStateSetOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
-import androidx.compose.runtime.snapshots.SnapshotStateList
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
+import com.letmecook.app.AnimatedFogGradientBackground
+import com.letmecook.app.AppColors
+import com.letmecook.app.MinimalGlowBackground
+import com.letmecook.app.TagChip
 import com.letmecook.app.client
 import io.ktor.client.call.body
 import io.ktor.client.plugins.resources.get
-import io.ktor.client.statement.HttpResponse
 import org.jetbrains.compose.ui.tooling.preview.Preview
-import kotlin.collections.mutableListOf
 
 var projects = mutableStateListOf<ProjectEntity>();
 
@@ -56,7 +49,8 @@ var projects = mutableStateListOf<ProjectEntity>();
 @Composable
 internal fun ProjectListView() {
 
-    var backendProjects by remember { mutableStateOf<List<ProjectDto>>(emptyList())}
+//    var backendProjects by remember { mutableStateOf<List<ProjectDto>>(emptyList())}
+    var backendProjects = Mocks.getAllProjects()
     LaunchedEffect(true){
         try {
             val response: List<ProjectDto> = client.get(Projects.All()).body()
@@ -69,13 +63,25 @@ internal fun ProjectListView() {
     var showDetailedProjectView by remember { mutableStateOf(false) }
     var clickedProject: ProjectDto? by remember { mutableStateOf(null) }
 
+    Box(Modifier.fillMaxSize()) {
+
+        MinimalGlowBackground(
+            modifier = Modifier.matchParentSize()
+        )
+
     Column(
-        modifier = Modifier.background(MaterialTheme.colorScheme.primaryContainer)
-            .fillMaxWidth(),
+        modifier = Modifier.fillMaxWidth(),
 
         horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.spacedBy(15.dp),
     ) {
+
+        Text(
+            "Hot projects",
+            style = MaterialTheme.typography.titleLarge,
+            modifier = Modifier.align(Alignment.Start),
+            color = AppColors.SecondaryFontColor
+        )
 
         clickedProject.let { project ->
             if (showDetailedProjectView && project != null) {
@@ -94,18 +100,29 @@ internal fun ProjectListView() {
 
                     ElevatedCard(
                         modifier = Modifier.height(200.dp).fillMaxWidth().clip(RoundedCornerShape(percent = 10))
-                            .background(Color.White)
+                            .background(AppColors.Primary),
+                        colors = CardDefaults.elevatedCardColors(
+                            containerColor = AppColors.Primary
+                        ),
                     ) {
                         Column(
                             modifier = Modifier.padding(15.dp)
                         ) {
-                            Text(project.id.toString())
                             Text(
                                 project.title,
                                 style = MaterialTheme.typography.titleLarge,
                                 modifier = Modifier.padding(0.dp, 0.dp, 0.dp, 5.dp),
+                                color = AppColors.SecondaryFontColor
                             )
-                            Text("Technical stack: ${project.tech_stack.joinToString(", ")} ")
+
+                            Text(project.description, color = AppColors.PrimaryFontColor, maxLines = 2, overflow = TextOverflow.Ellipsis)
+
+                            FlowRow(){
+                                project.tech_stack.map { unit ->
+                                    TagChip(unit.toString())
+                                }
+
+                            }
                             Text("Payment: ${project.payment_type} ")
 
                             Row(modifier = Modifier.fillMaxWidth(), horizontalArrangement = Arrangement.End) {
@@ -132,4 +149,5 @@ internal fun ProjectListView() {
 
     }
 
+    }
 }
