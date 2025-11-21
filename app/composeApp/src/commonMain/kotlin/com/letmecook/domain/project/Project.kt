7c4840b818423d3ef08f7766908a804a43cdf7a7
panevka <97028404+panevka@users.noreply.github.com>
2025-11-21 12:26:10 +0100

    feat: setup request dto & util for joining chosen group

--- app/composeApp/src/commonMain/kotlin/com/letmecook/domain/project/Project.kt
+++ app/composeApp/src/commonMain/kotlin/com/letmecook/domain/project/Project.kt
@@ -3,9 +3,13 @@ package com.letmecook.domain.project
 import io.ktor.client.HttpClient
 import io.ktor.client.call.body
 import io.ktor.client.plugins.resources.get
+import io.ktor.client.plugins.resources.post
+import io.ktor.client.request.setBody
 import io.ktor.client.statement.HttpResponse
+import io.ktor.http.ContentType
 import io.ktor.http.Headers
 import io.ktor.http.HttpStatusCode
+import io.ktor.http.contentType
 import io.ktor.resources.Resource
 import kotlinx.serialization.Serializable
 
@@ -13,6 +17,9 @@ import kotlinx.serialization.Serializable
 class Projects {
     @Resource("/all")
     class All(val parent: Projects = Projects())
+
+    @Resource("/join")
+    class Join(val parent: Projects = Projects())
 }
 
 @Serializable
@@ -20,3 +27,22 @@ data class ProjectDto(
     val id: Int,
     val title: String
 )
+
+@Serializable
+data class JoinProjectRequest(
+    val project_id: Int,
+    val user_id: Int
+)
+
+
+suspend fun joinProject(
+    client: HttpClient,
+    projectId: Int,
+    userId: Int
+): Boolean {
+    val response: HttpResponse = client.post(Projects.Join()) {
+        contentType(ContentType.Application.Json)
+        setBody(JoinProjectRequest(projectId, userId))
+    }
+    return response.status == HttpStatusCode.OK
+}
