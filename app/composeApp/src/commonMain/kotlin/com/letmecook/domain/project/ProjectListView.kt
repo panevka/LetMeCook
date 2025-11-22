183461374e519a6696c5ff74ebe5c1d9028f916c
panevka <97028404+panevka@users.noreply.github.com>
2025-11-22 15:24:51 +0100

    feat: add project icon, truncated description, title, author, prettify UI

--- app/composeApp/src/commonMain/kotlin/com/letmecook/domain/project/ProjectListView.kt
+++ app/composeApp/src/commonMain/kotlin/com/letmecook/domain/project/ProjectListView.kt
@@ -1,5 +1,6 @@
 package com.letmecook.domain.project
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
@@ -10,6 +11,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.requiredSize
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
@@ -30,16 +32,20 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
-import com.letmecook.app.AnimatedFogGradientBackground
+import app.composeapp.generated.resources.Res
+import app.composeapp.generated.resources.avatar
+import app.composeapp.generated.resources.project_icon
 import com.letmecook.app.AppColors
 import com.letmecook.app.MinimalGlowBackground
 import com.letmecook.app.TagChip
 import com.letmecook.app.client
 import io.ktor.client.call.body
 import io.ktor.client.plugins.resources.get
+import org.jetbrains.compose.resources.painterResource
 import org.jetbrains.compose.ui.tooling.preview.Preview
 
 var projects = mutableStateListOf<ProjectEntity>();
@@ -99,7 +105,7 @@ internal fun ProjectListView() {
                 items(it) { project ->
 
                     ElevatedCard(
-                        modifier = Modifier.height(200.dp).fillMaxWidth().clip(RoundedCornerShape(percent = 10))
+                        modifier = Modifier.height(250.dp).fillMaxWidth().clip(RoundedCornerShape(percent = 10))
                             .background(AppColors.Primary),
                         colors = CardDefaults.elevatedCardColors(
                             containerColor = AppColors.Primary
@@ -108,22 +114,40 @@ internal fun ProjectListView() {
                         Column(
                             modifier = Modifier.padding(15.dp)
                         ) {
+                            Row(modifier = Modifier.padding(bottom = 10.dp ), verticalAlignment = Alignment.CenterVertically){
+                            Image(
+                                painter = painterResource(Res.drawable.project_icon),
+                                contentDescription = "aha",
+                                contentScale = ContentScale.Fit,
+                                modifier = Modifier
+                                    .height(50.dp)
+                                    .padding(0.dp, 0.dp, 20.dp, 0.dp)
+                                    .clip(RoundedCornerShape(12.dp))
+                            )
+                                Column() {
                             Text(
                                 project.title,
                                 style = MaterialTheme.typography.titleLarge,
-                                modifier = Modifier.padding(0.dp, 0.dp, 0.dp, 5.dp),
                                 color = AppColors.SecondaryFontColor
                             )
 
-                            Text(project.description, color = AppColors.PrimaryFontColor, maxLines = 2, overflow = TextOverflow.Ellipsis)
+                                    Text("by Some Author", color = AppColors.PrimaryFontColor, maxLines = 2, overflow = TextOverflow.Ellipsis)
+                                }
 
-                            FlowRow(){
+                            }
+                            Text(project.description, color = AppColors.PrimaryFontColor, maxLines = 2, overflow = TextOverflow.Ellipsis, )
+
+                            FlowRow(
+                                modifier = Modifier.padding( bottom =10.dp, top=10.dp)
+
+                            ){
                                 project.tech_stack.map { unit ->
                                     TagChip(unit.toString())
                                 }
 
                             }
-                            Text("Payment: ${project.payment_type} ")
+
+                            Text("Payment: ${project.payment_type}", color = AppColors.PrimaryFontColor, maxLines = 2, overflow = TextOverflow.Ellipsis)
 
                             Row(modifier = Modifier.fillMaxWidth(), horizontalArrangement = Arrangement.End) {
                                 Button(onClick = {
