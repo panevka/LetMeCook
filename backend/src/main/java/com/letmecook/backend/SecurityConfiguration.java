e2095c2f18e57a90198cef145a4a0a9e63cc3aa9
panevka <97028404+panevka@users.noreply.github.com>
2025-11-24 14:28:07 +0100

    chore: rename security config file

---
+++ backend/src/main/java/com/letmecook/backend/SecurityConfiguration.java
@@ -0,0 +1,44 @@
+package com.letmecook.backend;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import lombok.AllArgsConstructor;
+
+@Configuration
+@AllArgsConstructor
+public class SecurityConfiguration {
+
+        private final AuthTokenFilter authTokenFilter;
+        private final AuthEntryPointJwt authEntryPointJwt;
+        private final OAuth2LoginSuccessHandler successHandler;
+
+        @Bean
+        public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+                http.csrf(csrf -> csrf.disable())
+                                .exceptionHandling(ex -> ex.authenticationEntryPoint(
+                                                authEntryPointJwt))
+                                .authorizeHttpRequests(auth -> auth
+                                                .requestMatchers("/test")
+                                                .authenticated()
+                                                .requestMatchers("/oauth2/**", "/loginSuccess",
+                                                                "/login/**")
+                                                .permitAll()
+                                                .requestMatchers("/api/**")
+                                                .authenticated()
+                                                .anyRequest()
+                                                .permitAll())
+                                .sessionManagement(sess -> sess
+                                                .sessionCreationPolicy(SessionCreationPolicy.IF_REQUIRED))
+                                .oauth2Login(oauth2 -> oauth2.successHandler(
+                                                successHandler));
+
+                http.addFilterBefore(authTokenFilter, UsernamePasswordAuthenticationFilter.class);
+
+                return http.build();
+        }
+}
