dd0d8548ae355633c736bd95905477b3ec0cb166
panevka <97028404+panevka@users.noreply.github.com>
2025-12-02 00:23:24 +0100

    feat: allow scroll of user profile

--- app/composeApp/src/commonMain/kotlin/com/letmecook/domain/user/UserView.kt
+++ app/composeApp/src/commonMain/kotlin/com/letmecook/domain/user/UserView.kt
@@ -9,7 +9,9 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.requiredSize
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
@@ -87,8 +89,11 @@ internal fun UserView () {
         val fullName = "${mockUser.first_name} ${mockUser.last_name}"
 
         Column(
-            modifier = Modifier.fillMaxSize().padding(30.dp),
+            modifier = Modifier.fillMaxSize().padding(30.dp).verticalScroll(
+                state = rememberScrollState()
+            ),
             horizontalAlignment = Alignment.CenterHorizontally
+
         ) {
 
             KamelImage(
