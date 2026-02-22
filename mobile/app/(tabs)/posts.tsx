860f3211815b44718ecbb38981ddd9e807c273de
panevka <97028404+panevka@users.noreply.github.com>
2026-02-22 19:04:22 +0100

    feat(mobile): add basic auth mechanism for routing

---
+++ mobile/app/(tabs)/posts.tsx
@@ -0,0 +1,5 @@
+import { Text } from "react-native";
+
+export default function Posts() {
+  return <Text>aha</Text>;
+}
--- mobile/app/_layout.tsx
+++ mobile/app/_layout.tsx
@@ -1,22 +1,31 @@
-import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
-import { Stack } from 'expo-router';
-import { StatusBar } from 'expo-status-bar';
-import 'react-native-reanimated';
+import {
+  DarkTheme,
+  DefaultTheme,
+  ThemeProvider,
+} from "@react-navigation/native";
+import { Stack } from "expo-router";
+import { StatusBar } from "expo-status-bar";
+import "react-native-reanimated";
 
-import { useColorScheme } from '@/hooks/use-color-scheme';
+import { useColorScheme } from "@/hooks/use-color-scheme";
 
-export const unstable_settings = {
-  anchor: '(tabs)',
-};
+type AuthStatus = "LOGGED_IN" | "UNAUTHORIZED";
+
+const authStatus: AuthStatus = "UNAUTHORIZED";
 
 export default function RootLayout() {
   const colorScheme = useColorScheme();
 
   return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
+    <ThemeProvider value={colorScheme === "dark" ? DarkTheme : DefaultTheme}>
       <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="modal" options={{ presentation: 'modal', title: 'Modal' }} />
+        <Stack.Protected guard={authStatus === "UNAUTHORIZED"}>
+          <Stack.Screen name="auth" />
+        </Stack.Protected>
+
+        <Stack.Protected guard={authStatus !== "UNAUTHORIZED"}>
+          <Stack.Screen name="(tabs)" />
+        </Stack.Protected>
       </Stack>
       <StatusBar style="auto" />
     </ThemeProvider>
