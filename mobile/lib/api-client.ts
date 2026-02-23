09033059f7b839c3fa7441fd2e9464aa64428b0c
panevka <97028404+panevka@users.noreply.github.com>
2026-02-23 22:42:50 +0100

    feat(mobile): configure api client

--- mobile/lib/api-client.ts
+++ mobile/lib/api-client.ts
@@ -2,4 +2,17 @@ import {
   QueryClient,
 } from '@tanstack/react-query'
 
+import { env } from "@/lib/env";
+
 export const queryClient = new QueryClient();
+
+const ENDPOINTS = {
+	authorize: "api/authorize/discord",
+} as const;
+
+export const API_URLS = Object.fromEntries(
+  Object.entries(ENDPOINTS).map(([key, path]) => [
+    key,
+    `${env.EXPO_PUBLIC_API_URL}${path}`,
+  ])
+) as Readonly<Record<keyof typeof ENDPOINTS, string>>;
